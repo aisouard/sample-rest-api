319fdbae4eef6653c09e8aeb35b488b3ace4c11f
Axel Isouard <axel@kidizz.com>
2019-06-08 12:52:25 +0200

    Move logger logic inside root index.js file

--- app/index.js
+++ app/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const morgan = require('morgan');
+const router = require('./routes');
 
 const app = express();
-
-app.use(morgan('combined'));
+app.use(router);
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const morgan = require('morgan');
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
+app.use(morgan('combined'));
 app.listen(port, () => {
   /* eslint-disable no-console */
   console.log(`app is listening on port ${port}.`);
