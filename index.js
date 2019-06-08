f92fcf4b2bad49d006fae3ff6e5ae20c7bc767a4
Axel Isouard <axel@kidizz.com>
2019-06-08 15:13:18 +0200

    Load the headtohead.json file upon server initialisation

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const morgan = require('morgan');
 const app = require('./app');
+const Player = require('./app/models/player');
+const { players } = require('./seed/headtohead');
 
 const port = process.env.PORT || 3000;
 
+Player.seed(players);
 app.use(morgan('combined'));
 app.listen(port, () => {
   /* eslint-disable no-console */
