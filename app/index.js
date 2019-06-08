20d745d61eb3cc077996da6e2665029ca2b02ed8
Axel Isouard <axel@kidizz.com>
2019-06-08 15:30:14 +0200

    Handle unknown routes and potential errors

--- app/index.js
+++ app/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = require('./routes');
 
 const app = express();
+
 app.use(router);
+app.use((req, res, next) => res.status(404).send());
+app.use((err, req, res, next) => res.status(500).send());
 
 module.exports = app;
